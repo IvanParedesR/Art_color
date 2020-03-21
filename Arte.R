6466a85f60bb8f0ce3fef7746a9b6336a1382107
Iván Paredes Reséndiz <ivanresendiz25@gmail.com>
2020-03-21 11:15:03 -0600

    Update Arte.R

--- Arte.R
+++ Arte.R
@@ -1,15 +1,18 @@
 library(readxl)
 BIE_BIE20200320185601 <- read_excel("C:/Users/iparedes/Downloads/Graphs/BIE_BIE20200320185601.xls", range = "A4:F164"))
+
+# Usando Cuentas nacionales > 
+# Producto interno bruto trimestral, base 2013 > Valores a precios de 2013 Producto Interno Bruto, a precios de mercado 
 # Libraries
 library(tidyverse)
 
-#z <- as.Date(BIE_BIE20200320185601$Periodo, "%Y%q")
+# Propuesta inicial, finalmente no se uso. z <- as.Date(BIE_BIE20200320185601$Periodo, "%Y%q")
 
-# Make the plot
+# Haz la gráfica
 p <- ggplot(BIE_BIE20200320185601, aes(x=as.factor(Periodo), y=PIB2013PM)) +       # Note that id is a factor. If x is numeric, there is some space between the first bar
   
-  # This add the bars with a blue color
-  geom_bar(stat="identity", fill=alpha("blue", 0.3)) +
+# Se llena de color azul
+ geom_bar(stat="identity", fill=alpha("blue", 0.3)) +
   
   # Limits of the plot = very important. The negative value controls the size of the inner circle, the positive one is useful to add size over each bar
   ylim(-10000000,24000000) +
