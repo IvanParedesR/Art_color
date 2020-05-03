4c6b1a8b2eac59e6b30d75068012df6916e1524f
Ivan Paredes <ivanresendiz25@gmail.com>
2020-05-02 23:09:20 -0500

    Local change

--- Arte.R
+++ Arte.R
@@ -28,10 +28,21 @@ p <- ggplot(BIE_, aes(x=as.factor(Periodo), y=PIB2013PM)) +
     panel.grid = element_blank(),
     plot.margin = unit(rep(-2,4), "cm")     # This remove unnecessary margin around plot
   ) +
-  
   # Se hacen coordenadas polares y no cartesianas
   coord_polar(start = -50)
 p
+
+BIE_$z<- ifelse((BIE_$Periodo >= "2018/04" & BIE_$Periodo <= "2019/04"), 7,+
+                  ifelse((BIE_$Periodo >= "2012/04" & BIE_$Periodo <= "2018/03"), 6,+
+                           ifelse((BIE_$Periodo >= "2006/04" & BIE_$Periodo <= "2012/03"), 5,+
+                                    ifelse((BIE_$Periodo >= "2000/04" & BIE_$Periodo <= "2006/03"), 4,+
+                                             ifelse((BIE_$Periodo >= "1994/04" & BIE_$Periodo <= "2000/03"), 3,+
+                                                      ifelse((BIE_$Periodo >= "1988/04" & BIE_$Periodo <= "1994/03"), 2,+
+                                                               ifelse((BIE_$Periodo >= "1982/04" & BIE_$Periodo <= "1988/03"), 1,+
+                                                                        ifelse((BIE_$Periodo >= "1980/04" & BIE_$Periodo <= "1982/03"), 0,+
+                                                                                 ifelse((BIE_$Periodo >= "1980/01" & BIE_$Periodo <= "1980/03"), 9, 8)))))))))
+
+BIE_$z <- as.factor(BIE_$z)
 
 # Aqui vamos poniendo un numero de barras en blanco para separar
 empty_bar <- 4
@@ -39,9 +50,9 @@ to_add <- data.frame( matrix(NA, empty_bar*nlevels(BIE_$z), ncol(BIE_)) )
 colnames(to_add) <- colnames(BIE_)
 to_add$z <- rep(levels(BIE_$z), each=empty_bar)
 BIE_ <- rbind(BIE_, to_add)
-BIE_ <- BIE_  %>% arrange(z)
-#BIE_ $P <- seq(1, nrow(z))
-BIE_$t <- 1:192
+BIE_ <- BIE_  %>%   arrange(z)
+BIE_ $P <- seq(1, nrow(BIE_))
+BIE_$t <- 1:196
 
 # Una nueva versión
 p <- ggplot(BIE_, aes(x=as.factor(t), y=PIB2013PM, fill=z)) +       # Note that id is a factor. If x is numeric, there is some space between the first bar
