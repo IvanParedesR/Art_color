5ab845603d9cbcc63a1e83d2a936657fb89ba7b9
Ivan Paredes <ivanresendiz25@gmail.com>
2020-05-10 20:57:07 -0500

    Local change

--- Arte.R
+++ Arte.R
@@ -73,34 +73,25 @@ p2
 BIE_2 <- c("Periodo", "PIB2013AP", "PIB2013AS", "PIB2013AT", "z")
 newdata <- BIE_[BIE_2]
 
-newdata %>%
+newdata <- newdata %>%
   pivot_longer(cols = c(`PIB2013AP`, `PIB2013AS`, `PIB2013AT`), names_to = "PIBxSectores", values_to = "PIB")
 
-p3 <- ggplot(BIE_) +      
+newdata$PIBxSectores <- as.factor(newdata$PIBxSectores)
+p3
+p3 <- ggplot(newdata) +      
   
   # Add the stacked bar
-  geom_bar(aes(x=as.factor(id), y=PIB, fill=observation), stat="identity", alpha=0.5) +
+  geom_bar(aes(x=as.factor(z), y=PIB, fill=PIBxSectores), stat="identity", alpha=0.5) +
   scale_fill_viridis(discrete=TRUE) +
   
   # Add a val=100/75/50/25 lines. I do it at the beginning to make sur barplots are OVER it.
-  geom_segment(data=grid_data, aes(x = end, y = 0, xend = start, yend = 0), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = end, y = 50, xend = start, yend = 50), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = end, y = 100, xend = start, yend = 100), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = end, y = 150, xend = start, yend = 150), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = end, y = 200, xend = start, yend = 200), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+  geom_segment(data=newdata, aes(x = end, y = 0, xend = start, yend = 0), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+  geom_segment(data=newdata, aes(x = end, y = 50, xend = start, yend = 50), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+  geom_segment(data=newdata, aes(x = end, y = 100, xend = start, yend = 100), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+  geom_segment(data=newdata, aes(x = end, y = 150, xend = start, yend = 150), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+  geom_segment(data=newdata, aes(x = end, y = 200, xend = start, yend = 200), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
   
-  # Add text showing the value of each 100/75/50/25 lines
-  ggplot2::annotate("text", x = rep(max(data$id),5), y = c(0, 50, 100, 150, 200), label = c("0", "50", "100", "150", "200") , color="grey", size=6 , angle=0, fontface="bold", hjust=1) +
-  
-  ylim(-150,max(label_data$tot, na.rm=T)) +
-  theme_minimal() +
-  theme(
-    legend.position = "none",
-    axis.text = element_blank(),
-    axis.title = element_blank(),
-    panel.grid = element_blank(),
-    plot.margin = unit(rep(-1,4), "cm") 
-  ) +
+  # Add  +
   coord_polar()
 
 p3
