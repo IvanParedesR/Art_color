1026ef0ab59cb6a6c35a6c19c17401b558cd6ba9
Ivan Paredes <ivanresendiz25@gmail.com>
2020-06-05 17:57:38 -0500

    local change

--- Arte 3.R
+++ Arte 3.R
@@ -30,11 +30,12 @@ library(osmdata)
 library(rgrass7)
 library(link2GI)
 library(nabor)
+library(magick)
 
-remotes::install_cran(cran_pkgs)
+# remotes::install_cran(cran_pkgs)
 
 # We choose Tacubaya a little neighborhood of Mexico City (el lugar donde nací)
-CDMX <- opq(bbox =  c(-99.2300, 19.391555, -99.1800, 19.40755)) %>% 
+CDMX <- opq(bbox =  c(-99.2000, 19.391555, -99.1800, 19.40755)) %>% 
   add_osm_feature(key = 'highway') %>% 
   osmdata_sf() %>% 
   osm_poly2line()
@@ -46,24 +47,62 @@ cdmx_center
 
 ggplot(data = cdmx_center) + geom_sf() + theme_bw()
 
-ggplot(data = cdmx_center) + geom_sf() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-
-library(grid)
-?opts
-ggplot(data = cdmx_center) + geom_sf() + opts(panel.background = theme_rect(fill='white', colour='black')) + opts(panel.grid.major = none, panel.grid.minor = none)
-
-png(tf <- tempfile(fileext = ".png"), 1000, 1000)
-par(mar = rep(0,4), yaxs="i", xaxs="i")
-plot(0, type = "n", ylim = c(0,1), xlim=c(0,1), axes=F, xlab=NA, ylab=NA)
-plotrix::draw.circle(.5,0.5,.5, col="black")
-dev.off()
+ggplot(data = cdmx_center) + geom_sf() + theme(panel.background = element_rect(fill = NA),panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
 
-library(magick)
-fn <- "https://www.gravatar.com/avatar/f57aba01c52e5c67696817eb87df84f2?s=328&d=identicon&r=PG&f=1"
-img <- image_read(fn)
+ggplot(data = cdmx_center) + geom_sf() + theme(axis.title.x=element_blank(), 
+                                               axis.text.x=element_blank(), 
+                                               axis.ticks.x=element_blank(),
+                                               axis.title.y=element_blank(), 
+                                               axis.text.y=element_blank(), 
+                                               axis.ticks.y=element_blank(),
+                                               panel.background = element_rect(fill = NA),
+                                               panel.grid.major = element_blank(), 
+                                               panel.grid.minor = element_blank())
+###########
+
+# We choose Tacubaya a little neighborhood of Mexico City (el lugar donde nací)
+CDMX <- opq(bbox =  c(-99.2000, 19.381555, -99.1800, 19.42755)) %>% 
+  add_osm_feature(key = 'highway') %>% 
+  osmdata_sf() %>% 
+  osm_poly2line()
+
+cdmx_center <- CDMX$osm_lines %>% 
+  select(highway)
+
+cdmx_center
+
+ggplot(data = cdmx_center) + geom_sf() + theme_bw()
+
+ggplot(data = cdmx_center) + geom_sf() + theme(panel.background = element_rect(fill = NA),panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
+
+imagen<- ggplot(data = cdmx_center) + geom_sf() + theme(axis.title.x=element_blank(), 
+                                               axis.text.x=element_blank(), 
+                                               axis.ticks.x=element_blank(),
+                                               axis.title.y=element_blank(), 
+                                               axis.text.y=element_blank(), 
+                                               axis.ticks.y=element_blank(),
+                                               panel.background = element_rect(fill = "blue"),
+                                               panel.grid.major = element_blank(), 
+                                               panel.grid.minor = element_blank())
+
+imagen
+frink <- image_read("~/Art_color/Rplot02.png")
+
+i <-image_crop(frink, "800x750+50")
+ia <-image_crop(i, "800x650-50")
+ia
+
+ib <-image_crop(ia, "500x590-350")
+ib
+
+ic <-image_crop(ib, "500x590-120")
+ic
+
+
+img <- image_read("~/Art_color/ib.png")
 mask <- image_read(tf)
 mask <- image_scale(mask, as.character(image_info(img)$width))
-mask
-img
+
 image_composite(mask, img, "plus") 
