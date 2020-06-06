bc0524a8ac5716b33edfca101d1445ee2f4dc32b
Ivan Paredes <ivanresendiz25@gmail.com>
2020-06-06 09:50:01 -0500

    local change

--- Arte 3.R
+++ Arte 3.R
@@ -90,7 +90,7 @@ imagen<- ggplot(data = cdmx_center) + geom_sf() + theme(axis.title.x=element_bla
 imagen
 frink <- image_read("~/Art_color/Rplot03.png")
 
-i <-image_crop(frink, "800x750+50")
+i <-image_crop(frink, "1800x750+50")
 ia <-image_crop(i, "800x650-50")
 ia
 
@@ -103,12 +103,28 @@ ic
 png(tf <- tempfile(fileext = ".png"), 1000, 1000)
 par(mar = rep(0,4), yaxs="i", xaxs="i")
 plot(0, type = "n", ylim = c(0,1), xlim=c(0,1), axes=F, xlab=NA, ylab=NA)
-plotrix::draw.circle(.5,0.5,.5, col="black")
+plotrix::draw.circle(.1,0.1,.1, col="black")
 dev.off()
 
 
 img <- image_read("~/Art_color/Rplot03.png")
+img
 mask <- image_read(tf)
 mask <- image_scale(mask, as.character(image_info(img)$width))
+mask
+image_composite(mask, img, "plus") 
 
+
+png(tf <- tempfile(fileext = ".png"), 1000, 1000)
+par(mar = rep(0,4), yaxs="i", xaxs="i")
+plot(0, type = "n", ylim = c(0,1), xlim=c(0,1), axes=F, xlab=NA, ylab=NA)
+plotrix::draw.circle(.5,0.5,.5, col="black")
+dev.off()
+
+fn <- "https://www.gravatar.com/avatar/f57aba01c52e5c67696817eb87df84f2?s=328&d=identicon&r=PG&f=1"
+img <- image_read(fn)
+img
+mask <- image_read(tf)
+mask <- image_scale(mask, as.character(image_info(img)$width))
+mask
 image_composite(mask, img, "plus") 
