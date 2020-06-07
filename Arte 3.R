37c6dd97568686742972a78000da6d797f4fd44c
Ivan Paredes <ivanresendiz25@gmail.com>
2020-06-07 12:54:19 -0500

    Local change

--- Arte 3.R
+++ Arte 3.R
@@ -1,7 +1,7 @@
 # Se instalan una serie de paquetes.
-if(!"remotes" %in% installed.packages()) {
-  install.packages("remotes")
-}
+#if(!"remotes" %in% installed.packages()) {
+#  install.packages("remotes")
+#}
 
 cran_pkgs = c(
   "sf",
@@ -88,7 +88,8 @@ imagen<- ggplot(data = cdmx_center) + geom_sf() + theme(axis.title.x=element_bla
                                                panel.grid.minor = element_blank())
 
 imagen
-frink <- image_read("~/Art_color/Rplot03.png")
+frink <- image_read("~/Art_color/Rplot04.png")
+frink
 
 i <-image_crop(frink, "1800x750+50")
 ia <-image_crop(i, "800x650-50")
@@ -100,19 +101,21 @@ ib
 ic <-image_crop(ib, "500x590-120")
 ic
 
-png(tf <- tempfile(fileext = ".png"), 1000, 1000)
+png(tf <- tempfile(fileext = ".png"), 2500, 2500)
 par(mar = rep(0,4), yaxs="i", xaxs="i")
 plot(0, type = "n", ylim = c(0,1), xlim=c(0,1), axes=F, xlab=NA, ylab=NA)
-plotrix::draw.circle(.1,0.1,.1, col="black")
+plotrix::draw.circle(.5,0.5,.5, col="black")
 dev.off()
 
 
-img <- image_read("~/Art_color/Rplot03.png")
+img <- image_read("~/Art_color/Rplot04.png")
 img
 mask <- image_read(tf)
 mask <- image_scale(mask, as.character(image_info(img)$width))
 mask
 image_composite(mask, img, "plus") 
+
+image_composite(mask, img, "minus") 
 
 
 png(tf <- tempfile(fileext = ".png"), 1000, 1000)
